08fdb49408a5ff0955916b18fd0284dcee6e9bd6
ChitranshBP <chitransh@brandingpioneers.co.in>
2026-07-29 15:45:10 +0530

    Configure deployment target to drsanjayswamy.co.in

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://drsanjayswamy.com',
+  site: 'https://drsanjayswamy.co.in',
   integrations: [sitemap()],
 });
